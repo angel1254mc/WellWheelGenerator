d43eedbb10a5cf992ecf37c788b8c4b11717620d
Angel Lopez Pol <59091763+angel1254mc@users.noreply.github.com>
2022-05-22 17:50:43 -0400

    Created Initial React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World!
+        </p>
+        <a
+          className="App-link"
+          href="https://youtu.be/dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Do not click on this
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
